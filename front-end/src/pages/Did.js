20fa68980508dbcdf6f2868c8e83be3169574995
Patrik <patrik.marcok@student.tuke.sk>
2023-05-07 16:18:14 +0200

    UI of did.js

--- front-end/src/pages/Did.js
+++ front-end/src/pages/Did.js
@@ -3,10 +3,25 @@ import web3 from '../web3';
 
 // Use web3 to interact with the Ethereum network
 export function DID() {
-  
+
     return (
       <div>
         <h1>My dApp</h1>
+        <p>Hi there</p>
+       <form>
+            <label>
+                Visible setting update:
+                <input type="text" value={"address"} ></input>
+            </label>
+            <button type="submit">Update</button>
+            </form>
+            <form>
+            <label>
+                Private setting update:
+                <input type="text" value={"score"}></input>
+            </label>
+            <button type="submit">Update</button>
+        </form> 
       </div>
     );
   }
