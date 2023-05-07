a2369bcc8a8d627c5963446e8cd726c29573c213
Patrik <patrik.marcok@student.tuke.sk>
2023-05-07 16:37:08 +0200

    UI of did.js

--- front-end/src/pages/Did.js
+++ front-end/src/pages/Did.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import web3 from '../web3';
+const contracts = require("../contracts.js");
 
 // Use web3 to interact with the Ethereum network
 export function DID() {
 
+
+  const [privateSettings, setPrivate] = useState("");
+  const [publicSettings, setPublic] = useState("");
     return (
       <div>
         <h1>My dApp</h1>
        <form>
             <label>
                 Visible setting update:
-                <input type="text" value={""} ></input>
+                <input type="text" value={publicSettings} onChange={(e) => setPublic(e.target.value)} />
             </label>
             <button type="submit">Update</button>
-            </form>
-            <form>
-            <label>
+        </form>
+        <form>
+           <label>
                 Private setting update:
-                <input type="text" value={""}></input>
+                <input type="text" value={privateSettings} onChange={(e) => setPrivate(e.target.value)}/>
             </label>
             <button type="submit">Update</button>
         </form> 
