ff1492b46eb9a66b92f618c34d527fbd2e175c8d
JeznaSpianca <jurij.kolenik@gmail.com>
2023-05-12 22:43:23 +0200

    Forum change and deploy info

--- contracts/Forum.sol
+++ contracts/Forum.sol
@@ -22,8 +22,8 @@ contract Forum {
     mapping(uint256 => Thread) public threads;
 
     function createThread(string memory _title, uint256 _poolId) public {
-        threadCount++;
         Thread storage thread = threads[threadCount];
+        threadCount++;
         thread.title = _title;
         thread.creator = msg.sender;
         thread.messageCount = 0;
@@ -33,7 +33,6 @@ contract Forum {
     function postMessage(uint256 _threadId, string memory _content) public {
         require(_threadId <= threadCount, "Invalid thread id");
         Thread storage thread = threads[_threadId];
-        require(thread.id != 0, "Thread does not exist");
         thread.messageCount++;
         thread.messages[thread.messageCount] = Message(thread.messageCount, _content, msg.sender, block.timestamp);
     }
@@ -41,7 +40,6 @@ contract Forum {
     function getThread(uint256 _threadId) public view returns (Message[] memory) {
         require(_threadId <= threadCount, "Invalid thread id");
         Thread storage thread = threads[_threadId];
-        require(thread.id != 0, "Thread does not exist");
 
         Message[] memory messages = new Message[](thread.messageCount);
         for (uint256 i = 1; i <= thread.messageCount; i++) {
