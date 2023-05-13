d34ef7232d51ddd4339d8505cb0bbbaf4767ac42
JeznaSpianca <jurij.kolenik@gmail.com>
2023-05-13 16:50:59 +0200

    Betting frontend interactions

---
+++ front-end/src/pages/Betting.js
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import web3 from '../web3';
+const contracts = require("../contracts.js");
+
+// Use web3 to interact with the Ethereum network
+export function Betting() {
+  
+    async function getWith() {
+        const accounts = await web3.eth.getAccounts();
+        const contractAddress = contracts.betting_contract.address;
+        const contractABI = contracts.betting_contract.abi;
+    
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const withdraw_am = await contract.methods.balances(accounts[0]).call();
+    
+        setWitAm(withdraw_am);
+    }
+    const [text, setText] = useState("");
+
+
+    const [address, setAddress] = useState("");
+    const [score, setScore] = useState("");
+    const [status, setStatus] = useState("");
+
+
+    const [address_get, setAddress_get] = useState("");
+    const [reputation, setReputation] = useState("");
+    const [status_get, setStatus_get] = useState("");
+
+    const [pool, setPool] = useState("");
+    const [pool_id, setPoolID] = useState("");
+    const [amount, setAmount] = useState("");
+    const [isVisible, setIsVisible] = useState(false);
+    const [pool_id_settle, setPoolIDSettle] = useState("");
+    const [with_amount, setWitAm] = useState("");
+
+    const [reqID, setReqID] = useState("");
+    const [reqInd, setReqInd] = useState("");
+
+    const handleSubmitGetPool = async (event) => {
+        event.preventDefault();
+    
+        const contractAddress = contracts.betting_contract.address;
+        const contractABI = contracts.betting_contract.abi;
+    
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const pool = await contract.methods.bettingPools(pool_id).call();
+    
+        setPool(pool);
+        setIsVisible(true);
+      };
+  
+    const handleSubmitPlaceBet = async (event) => {
+      event.preventDefault();
+      const contractAddress = contracts.betting_contract.address;
+      const contractABI = contracts.betting_contract.abi;
+  
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      const accounts = await web3.eth.getAccounts();
+  
+      setStatus("Submitting transaction...");
+  
+      try {
+        await contract.methods.placeBet(pool_id, score).send({ from: accounts[0], value: amount });
+        setStatus("Transaction successful!");
+      } catch (error) {
+        console.error(error);
+        setStatus("Transaction failed.");
+      }
+    };
+
+    const handleSubmitSettleBetPool = async (event) => {
+        event.preventDefault();
+        const contractAddress = contracts.betting_contract.address;
+        const contractABI = contracts.betting_contract.abi;
+    
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const accounts = await web3.eth.getAccounts();
+    
+        setStatus("Submitting transaction...");
+    
+        try {
+          await contract.methods.settleBettingPool(pool_id,reqID,reqInd).send({ from: accounts[0] });
+          setStatus("Transaction successful!");
+        } catch (error) {
+          console.error(error);
+          setStatus("Transaction failed.");
+        }
+      };
+
+      const handleWithdraw = async (event) => {
+        event.preventDefault();
+        const contractAddress = contracts.betting_contract.address;
+        const contractABI = contracts.betting_contract.abi;
+    
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        const accounts = await web3.eth.getAccounts();
+    
+        setStatus("Submitting transaction...");
+    
+        try {
+          await contract.methods.withdraw().send({ from: accounts[0] });
+          setStatus("Transaction successful!");
+        } catch (error) {
+          console.error(error);
+          setStatus("Transaction failed.");
+        }
+      };
+
+    return (
+      <div>
+        <h1>Place bet</h1>
+
+        <form onSubmit={handleSubmitPlaceBet}>
+            <label>
+                Pool ID:
+                <input type="number" value={pool_id} onChange={(e) => setPoolID(e.target.value)} />
+            </label>
+            <label>
+                Outcome (0: hometeam win, 1: awayteam win, 2: draw):
+                <input type="number" value={score} onChange={(e) => setScore(e.target.value)} />
+            </label>
+            <label>
+                bet amount in wei:
+                <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+            </label>
+            <button type="submit">Submit</button>
+            <p>{status}</p>
+        </form>
+
+        <h1>Get pool</h1>
+        <form onSubmit={handleSubmitGetPool}>
+            <label>
+                Pool ID:
+                <input type="number" value={pool_id} onChange={(e) => setPoolID(e.target.value)} />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+            {isVisible && <div id="pool" visible="false">
+            <p>Pool name: {pool.name}</p>
+            <p>Total bets: {pool.totalBets}</p>
+            <p>Total amount in pool: {pool.totalAmount}</p>
+            <p>Pool outcome (4 if not settled): {pool.outcome}</p>
+            <p>Pool req ID: {pool.requestID}</p>
+            <p>Req index: {pool.req_index}</p>
+            <p>Game ID: {pool.gameID}</p>
+            <p>Owner: {pool.owner}</p>
+            <p>Cons contract: {pool.cons_contract}</p>
+            <p>Owner fee: {pool.ownerFee}%</p>
+            </div>}
+
+        <h1>Settle pool (only pool owner!)</h1>
+        <form onSubmit={handleSubmitSettleBetPool}>
+            <label>
+                Pool ID:
+                <input type="number" value={pool_id_settle} onChange={(e) => setPoolIDSettle(e.target.value)} />
+                Req ID:
+                <input type="text" value={reqID} onChange={(e) => setReqID(e.target.value)} />
+                Req index:
+                <input type="number" value={reqInd} onChange={(e) => setReqInd(e.target.value)} />
+                <button type="submit">Submit</button>
+                <p>{status}</p>
+            </label>
+        </form>
+
+        <h1>Withdraw</h1>
+        <p>You can withdraw {with_amount} wei <button onClick={getWith}>Get Amount available to withdraw</button></p>
+
+        <button onClick={handleWithdraw}>Withdraw</button>
+        <p>{status}</p>
+      </div>
+      
+    );
+  }
+  
+  
